40009b152632b4e72494039ee0fd0c4b988a2518
Omkar Waghmare <waghmareomkar77@gmail.com>
2024-09-30 12:03:05 +0530

    Java Learning

---
+++ Primitive_DataType/Integer_litreals.java
@@ -0,0 +1,15 @@
+public class Integer_litreals {
+    public static void main(String[] args) {
+        int num = 29;
+        int decimal = 29;        
+        int binary = 0b11101;    
+        int octal = 035;         
+        int hex = 0x1D;          
+
+        System.out.println("num: " + num);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Binary: " + binary);
+        System.out.println("Octal: " + octal);
+        System.out.println("Hexadecimal: " + hex);
+    }
+}
---
+++ Primitive_DataType/Print_Default_value.java
@@ -0,0 +1,24 @@
+public class Print_Default_value {
+
+    static int i;
+    static String s;
+    static float f;
+    static boolean b;
+    static double d;
+    static short sh;
+    static char c;
+    static byte by;
+
+    public static void main(String[] args) {
+        
+        System.out.println("int: " + i);
+        System.out.println("String : "+s);
+        System.out.println("float : "+f);
+        System.out.println("boolean : "+b);
+        System.out.println("double : "+d);
+        System.out.println("short : "+sh);
+        System.out.println("char : "+ c);
+        System.out.println("byte : "+by);
+
+    }
+}
